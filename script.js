2e32d86d75c6be44f05e04d645963f6d05efb06b
Kathy Bradt <kathybradt717@gmail.com>
2019-06-27 14:26:02 -0500

    sorted out button clicking problems

--- script.js
+++ script.js
@@ -4,15 +4,54 @@ $('#who').css({"border": "2px solid rgba(201, 105, 201, 0.623)"});
 
 $(document).ready(function(){
     
-
     $('.hamburger').on('click', event =>{
+        console.log ('burger clicked');
         $('.bar1').toggleClass('change');
         $('.bar2').toggleClass('change');
         $('.bar3').toggleClass('change');
-        $('nav').toggleClass('hidden');
+        $('nav.burger').slideToggle("slow");
+        burgerClick();
     });
 
+    $('.hidden button').on('click', event=>{
+        console.log ('hidden clicked');
+        hiddenClick();
+    });
+
+function hiddenClick(){
+    $(".hidden #who").on('click', event =>{
+        console.log ('who clicked');
+        $('.who').show();
+        $('.talk').hide();
+        $('.what').hide();
+        $('#who').css({"border": "2px solid rgba(201, 105, 201, 0.623)"});
+        $('#talk').css({'border':'none'});
+        $('#what').css({'border':'none'});
+    });
+
+    $(".hidden #talk").on('click', event =>{
+        console.log ('talk clicked');
+        $('.who').hide();
+        $('.talk').show();
+        $('.what').hide();
+        $('#who').css({'border':'none'});
+        $('#talk').css({"border": "2px solid rgba(201, 105, 201, 0.623)"});
+        $('#what').css({'border':'none'});
+    });
+
+    $(".hidden #what").on('click', event =>{
+        console.log ('what clicked');
+        $('.who').hide();
+        $('.talk').hide();
+        $('.what').show();
+        $('#who').css({'border':'none'});
+        $('#talk').css({'border':'none'});
+        $('#what').css({"border": "2px solid rgba(201, 105, 201, 0.623)"});
+    });
+}
+function burgerClick(){
     $("#who").on('click', event =>{
+        console.log ('who clicked');
         $('.who').show();
         $('.talk').hide();
         $('.what').hide();
@@ -22,6 +61,7 @@ $(document).ready(function(){
     });
 
     $("#talk").on('click', event =>{
+        console.log ('talk clicked');
         $('.who').hide();
         $('.talk').show();
         $('.what').hide();
@@ -31,6 +71,7 @@ $(document).ready(function(){
     });
 
     $("#what").on('click', event =>{
+        console.log ('what clicked');
         $('.who').hide();
         $('.talk').hide();
         $('.what').show();
@@ -38,4 +79,5 @@ $(document).ready(function(){
         $('#talk').css({'border':'none'});
         $('#what').css({"border": "2px solid rgba(201, 105, 201, 0.623)"});
     });
+}
     });
