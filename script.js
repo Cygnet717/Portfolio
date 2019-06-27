93d165a57256c3bf51b2f15b8dbb65a72ca607a8
Kathy Bradt <kathybradt717@gmail.com>
2019-06-27 13:27:26 -0500

    revised small screen nav bar to hamburger

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+$('.talk').hide();
+$('.what').hide();
+$('#who').css({"border": "2px solid rgba(201, 105, 201, 0.623)"});
+
 $(document).ready(function(){
-    $('.talk').hide();
-    $('.what').hide();
-    $('#who').css({"border": "2px solid rgba(201, 105, 201, 0.623)"});
     
+
+    $('.hamburger').on('click', event =>{
+        $('.bar1').toggleClass('change');
+        $('.bar2').toggleClass('change');
+        $('.bar3').toggleClass('change');
+        $('nav').toggleClass('hidden');
+    });
+
     $("#who").on('click', event =>{
         $('.who').show();
         $('.talk').hide();
